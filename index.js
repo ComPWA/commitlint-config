d2ee90787ac29df6dc35c126c3d208b1ca324122
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-10-05 11:29:26 +0200

    BEHAVIOR: limit number of characters in commit title

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
+    "header-max-length": [2, "always", 72],
     "type-case": [2, "always", ["upper-case"]],
     "type-enum": [
       2,
