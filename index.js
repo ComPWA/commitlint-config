d1b46ae788eca59812e59903a4715bd58f5a978b
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-01-29 11:41:24 +0100

    FIX: pin `@commitlint/config-conventional` in `package.json`

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional@v18.4.4"],
+  extends: ["@commitlint/config-conventional"],
   rules: {
     "header-max-length": [2, "always", 72],
     "type-case": [2, "always", ["upper-case"]],
